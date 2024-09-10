45b543297c01621b4a9fe19ed71c86bd5266255b
Serge <serge-mur@yandex.ru>
2024-09-10 17:05:09 +0300

    slider, start info

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+    const swiper = new Swiper('.main-slider', {
+        loop: true,
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+    });
 })
